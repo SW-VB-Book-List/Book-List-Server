8348f3f0f9f382d30d14aa2d71ea857725b829f7
Victor Bofill <terranvas@gmail.com>
2018-03-12 15:24:02 -0700

    Created database controls

--- db-client.js
+++ db-client.js
@@ -1,15 +1,19 @@
-const env = require('dotenv').config();
+const dotenv = require('dotenv')
+dotenv.config();
+
 const pg = require('pg');
 
 
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/books';
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:wastu3eg@localhost:5432/books';
 
 const conString = 'DATABASE_URL';
 const client = new pg.Client(DATABASE_URL);
 
-client.connect();
+client.connect()
+    .then(() => console.log('connected to db', DATABASE_URL))
+    .catch(err => console.error('connection error', err));
 
-client.on('error', err => 'error');
+client.on('error', err => console.error(err));
 
 module.exports = client;
 
---
+++ load-db/drop-tables.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const client = require('../db-client');
+
+client.query(`
+    DROP TABLE IF EXISTS books;
+`).then(
+    () => console.log('db task successful!'),
+    err => console.error(err)
+).then(() => client.end());
